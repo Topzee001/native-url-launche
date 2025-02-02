63b168dc97305b6edde703f3cb9c2406f022a3f2
Ibrahim <ibrahimsakariyah@gmail.com>
2025-02-02 19:19:30 +0100

    constants created

---
+++ lib/utils/app_links.dart
@@ -0,0 +1,7 @@
+class AppLinks {
+  static const String githubRepo =
+      'https://github.com/Topzee001/native-url-launche';
+  static const String hireDev = 'http://hng.tech/hire/flutter-developers';
+  static const String telex = 'https://telex.im/';
+  static const String delve = 'https://delve.fun/';
+}
